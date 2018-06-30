cfd00c93eb4219b240b0c166d89e67c384e44526
freakinacast <m.vinesh@gmail.com>
2018-06-30 11:50:02 +0300

    Added the MVC folders

---
+++ TODOEY/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  TODOEY
+//
+//  Created by vinesh on 24/06/2018.
+//  Copyright © 2018 vinesh. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
